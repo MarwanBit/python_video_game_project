9a242b472873f29bbde1a522d0fa93a69c29e2fa
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-03 05:50:00 -0500

    Initial commit

---
+++ ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ship():
+
+	def __init__(self, screen):
+		'''initialize the ship and it's starting position'''
+		self.screen = screen
+
+		#Load the ship image and get its rect
+		self.image = pygame.image.load('C:/Users/Shadow/Documents/GitHub/python_video_game_project/images/ship.bmp')
+		self.rect = self.image.get_rect()
+		self.screen_rect = screen.get_rect()
+
+		#Start each new ship at the bottom center of the screen
+		self.rect.centerx = self.screen_rect.centerx
+		self.rect.bottom = self.screen_rect.bottom
+
+	def blitme(self):
+		'''Draw the ship at its current location'''
+		self.screen.blit(self.image, self.rect)
+
